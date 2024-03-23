7655dc0f670e03af6479a3a3b186cb7763e299b4
DJ HIP HOUSE <83514809+Djhiphouse@users.noreply.github.com>
2024-03-23 14:09:21 +0100

    Added SqlBuilder

--- Project/LaraSharp/Program.cs
+++ Project/LaraSharp/Program.cs
@@ -1,13 +1,21 @@
 ﻿using System;
 using System.Runtime.InteropServices;
 using System.Threading;
+using System.Threading.Tasks;
 using Adventofcode_day1.Server;
 using Adventofcode_day1.Settings;
+using Adventofcode_day1.Sql;
 
 class Program
 {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
+        SqlBuilder builder = new SqlBuilder("localhost", "3006", "root", "ru[i43uxm0DY&4Tj}Z\"3|/5?^!R|c,8%((VQ:T_z&@tJU", "test");
+        Sql sql = new Sql(builder.Database, builder.Connection);
+        string conetent =  await sql.Read("base", new string[] { "test" });
+        
+        Console.WriteLine(conetent);
+        
         Settings settings = new Settings();
         settings.Initializ();
         settings.SetSettings();
--- Project/LaraSharp/Sql/SqlBuilder.cs
+++ Project/LaraSharp/Sql/SqlBuilder.cs
@@ -1,7 +1,122 @@
+using System;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using Mysqlx.Datatypes;
+
 namespace Adventofcode_day1.Sql
 {
     public class SqlBuilder
     {
+        public string Ip { get; set; }
+        public string Port { get; set; }
+        public string User { get; set; }
+        public string Password { get; set; }
+        public string Database { get; set; }
         
+        public MySqlConnection Connection { get; set; }
+        
+        public SqlBuilder(string ip, string port, string user, string password, string database)
+        {
+            Ip = ip;
+            Port = port;
+            User = user;
+            Password = password;
+            Database = database;
+            Connection = new MySqlConnection(BuildConnection());
+        }
+        
+        private string BuildConnection()
+        {
+            return $"Server={Ip},{Port};Database={Database};User Id={User};Password={Password};";
+        }
+        
+    }
+    public class Sql
+    {
+        public string command { get; set; }
+        public MySqlConnection Connection;
+        public Sql(string database, MySqlConnection con)
+        {
+            Connection = con;
+            command += $"USE {database}; ";
+        }
+        
+        public void Create(string table, string[] columns, string[] values)
+        {
+            command += $"INSERT INTO {table} (";
+            for (int i = 0; i < columns.Length; i++)
+            {
+                command += $"{columns[i]}, ";
+            }
+            command = command.Remove(command.Length - 2);
+            command += ") VALUES (";
+            for (int i = 0; i < values.Length; i++)
+            {
+                command += $"'{values[i]}', ";
+            }
+            command = command.Remove(command.Length - 2);
+            command += "); ";
+        }
+        
+        public async Task<string> Read(string table, string[] columns)
+        {
+            command += $"SELECT ";
+            for (int i = 0; i < columns.Length; i++)
+            {
+                command += $"{columns[i]}, ";
+            }
+            command = command.Remove(command.Length - 2);
+            command += $" FROM {table}; ";
+            return await ExecuteRead(columns);
+        }
+        
+        public void Open()
+        {
+            Connection.Open();
+        }
+        
+        public void Close()
+        {
+            Connection.Close();
+        }
+        
+        public async Task Execute()
+        {
+            Open();
+            MySqlCommand sqlCommand = new MySqlCommand(command, Connection);
+            await sqlCommand.ExecuteNonQueryAsync();
+            Close();
+        }
+        
+        public async Task<string> ExecuteRead(string[] Coluums)
+        {
+            string result = "";
+
+            try
+            {
+                Open();
+                MySqlCommand val = new MySqlCommand(command, Connection);
+                MySqlDataReader Data = val.ExecuteReader();
+                await ((DbDataReader)(object)Data).ReadAsync();
+                if (!((DbDataReader)(object)Data).HasRows)
+                {
+                    return "No rows found";
+                }
+
+                foreach (var coluum in Coluums)
+                {
+                    result += Environment.NewLine +  Data.GetString(coluum);  
+                }
+                
+            }
+            finally
+            {
+                Close();
+            }
+
+            return result.TrimEnd(); // Trim the last newline for cleaner output
+        }
     }
 }
